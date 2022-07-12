741d605229d713444cc8dda0d56ce0d5019f71c5
Blaumeise03 <bluegame61@gmail.com>
2022-07-12 14:52:37 +0200

    Improved error handling/logging

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
+import traceback
 from datetime import datetime
+import logging
 import json
 import os
 import sys
 import time
 from os.path import exists
-import logging
 
 import discord
 from discord.ext import commands
@@ -16,10 +17,20 @@ log_filename = "logs/" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".log"
 print("Logging outputs goes to: " + log_filename)
 if not os.path.exists("logs/"):
     os.mkdir("logs")
-logging.basicConfig(filename=log_filename, filemode="a",
-                    format="[%(asctime)s][%(levelname)s][%(name)s]: %(message)s",
-                    level=logging.INFO)
-logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+formatter = logging.Formatter(fmt="[%(asctime)s][%(levelname)s][%(name)s]: %(message)s")
+# logging.basicConfig(filename=log_filename, filemode="a",
+#                    format="[%(asctime)s][%(levelname)s][%(name)s]: %(message)s",
+#                    level=logging.INFO)
+file_handler = logging.FileHandler(log_filename)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+logging.getLogger().addHandler(file_handler)
+console = logging.StreamHandler(sys.stdout)
+console.setLevel(logging.INFO)
+console.setFormatter(formatter)
+logging.getLogger().addHandler(console)
+logging.getLogger().setLevel(logging.INFO)
+
 # loading env
 logging.info("Loading .env...")
 load_dotenv()
@@ -54,11 +65,28 @@ else:
         json.dump(config, outfile, indent=4)
         logging.error("ERROR: Config not found, created new one. Please change the settings and restart!")
 
-
 logging.info("Starting up bot...")
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="§", intents=intents)
+
+
+@bot.event
+async def on_error(event_name, *args, **kwargs):
+    logging.exception("Error:")
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if ctx.guild is not None:
+        logging.error(
+            "Error in guild " + str(ctx.guild.id) + " in channel " + str(ctx.channel.id) +
+            ", sent by " + str(ctx.author.id) + ": " + ctx.author.name)
+    else:
+        logging.error(
+            "Error outside of guild in channel " + str(ctx.channel.id) +
+            ", sent by " + str(ctx.author.id) + ": " + ctx.author.name)
+    logging.exception(error, exc_info=False)
 
 
 @bot.event
@@ -85,37 +113,50 @@ async def on_message(message):
 @bot.command()
 async def setup(ctx):
     global MENU_MESSAGE, MENU_CHANNEL, GUILD
+    logging.info("Setup command called by user " + str(ctx.author.id))
     if ctx.guild is None:
+        logging.info("Command was send via DM!")
         await ctx.send("Can only be executed inside a guild")
     elif ctx.guild.id == GUILD or ctx.author.id == 485518598517948416:
         if ctx.author.guild_permissions.administrator or ctx.author.id == 485518598517948416:
-            logging.warning("Setup")
+            logging.info("User verified, starting setup...")
             view = AccountingView(ctx=ctx, bot=bot, accounting_log=ACCOUNTING_LOG)
             msg = await ctx.send(view=view, embeds=get_embeds())
+            logging.info("Send menu message with id " + str(msg.id))
             MENU_MESSAGE = msg.id
             MENU_CHANNEL = ctx.channel.id
             GUILD = ctx.guild.id
             save_config()
+            logging.info("Setup completed.")
             await ctx.send("Saved config")
         else:
+            logging.info("Missing perms!")
             await ctx.send("Missing permissions")
     else:
+        logging.info("Wrong server!")
         await ctx.send("Wrong server")
+
 
 # noinspection SpellCheckingInspection
 @bot.command()
 async def setlogchannel(ctx):
     global ACCOUNTING_LOG
+    logging.info("SetLogChannel command received.")
     if ctx.guild is None:
-        ctx.send("Only available inside a guild")
+        logging.info("Command was send via DM!")
+        await ctx.send("Only available inside a guild")
     elif ctx.guild.id == GUILD:
         if ctx.author.id == 485518598517948416 or ctx.author.guild_permissions.administrator:
+            logging.info("User Verified. Setting up channel...")
             ACCOUNTING_LOG = ctx.channel.id
             save_config()
+            logging.info("Channel changed!")
             await ctx.send("Log channel set to this channel (" + str(ACCOUNTING_LOG) + ")")
         else:
+            logging.info("Missing perms!")
             await ctx.send("Missing permissions")
     else:
+        logging.info("Wrong server!")
         await ctx.send("Can only used inside the defined discord server")
 
 
@@ -131,14 +172,17 @@ async def stop(ctx):
 
 
 def save_config():
+    logging.warning("Saving config...")
     global outfile
     config["server"] = GUILD
     config["logChannel"] = ACCOUNTING_LOG
     config["menuMessage"] = MENU_MESSAGE
     config["menuChannel"] = MENU_CHANNEL
+    logging.info("Config dict updated. Writing to file...")
     with open("config.json", "w") as outfile:
         json.dump(config, outfile, indent=4)
         logging.warning("Config saved!")
+    logging.info("Save completed.")
 
 
 bot.run(TOKEN)
