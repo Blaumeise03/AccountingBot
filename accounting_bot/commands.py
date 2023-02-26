7b2515767ef387f59c5cb131e3fcef73ca7d7117
Blaumeise03 <bluegame61@gmail.com>
2023-02-26 20:12:34 +0100

    Improved help command.

--- accounting_bot/commands.py
+++ accounting_bot/commands.py
@@ -1,7 +1,7 @@
 import asyncio
 import io
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List, Optional, Union
 
 import discord
 from discord import Option, User, ApplicationContext, AutocompleteContext, option, Role, SlashCommand, SlashCommandGroup
@@ -24,6 +24,10 @@ def main_char_autocomplete(self: AutocompleteContext):
     return filter(lambda n: self.value is None or n.startswith(self.value.strip()), utils.main_chars)
 
 
+def get_cmd_name(cmd: Union[commands.Command, discord.ApplicationCommand]):
+    return f"{cmd.full_parent_name} {cmd.name}".strip()
+
+
 class HelpCommand(commands.Cog):
     def __init__(self, state: 'BotState'):
         self.state = state
@@ -33,98 +37,114 @@ class HelpCommand(commands.Cog):
         for name, cog in self.state.bot.cogs.items():
             cmds.append(name)
             for cmd in cog.walk_commands():
-                cmds.append(f"{cmd.full_parent_name} {cmd.name}".strip())
+                cmds.append(f"{get_cmd_name(cmd)}")
         for cmd in self.state.bot.commands:
-            cmds.append(f"{cmd.full_parent_name} {cmd.name}".strip())
+            cmds.append(f"{get_cmd_name(cmd)}".strip())
         return filter(lambda n: ctx.value is None or n.casefold().startswith(ctx.value.casefold().strip()), cmds)
 
-    @commands.slash_command(name="help", description="Help-Command")
-    @option("selection", description="The command/module to get help about.", required=False, default=None,
-            autocomplete=commands_autocomplete)
-    @option("not_silent", description="Execute the command publicly.", required=False, default=False,
-            autocomplete=commands_autocomplete)
-    async def help(self, ctx: ApplicationContext,
-                   selection: str, not_silent: bool):
-        if selection is None:
-            emb = discord.Embed(title="Hilfe", color=discord.Color.red(),
-                                description="Gib `/help <Befehl>` oder `/help <Modul>` ein für weitere Informationen")
-            for name, cog in self.state.bot.cogs.items():  # type: str, commands.Cog
-                cmd_desc = ""
-                for cmd in cog.get_commands():
-                    cmd_desc += f"`{cmd.name}`: {cmd.description}\n"
-                emb.add_field(name=name, value=cmd_desc, inline=False)
-
+    @staticmethod
+    def get_general_embed(bot: commands.Bot):
+        emb = discord.Embed(title="Hilfe", color=discord.Color.red(),
+                            description="Gib `/help <Befehl>` oder `/help <Modul>` ein für weitere Informationen")
+        for name, cog in bot.cogs.items():  # type: str, commands.Cog
             cmd_desc = ""
-            for command in self.state.bot.walk_commands():
-                # if cog not in a cog
-                # listing command if cog name is None and command isn't hidden
-                if not command.cog_name and not command.hidden:
-                    cmd_desc += f'{command.name} - {command.description}\n'
-
-            if cmd_desc:
-                emb.add_field(name="Andere Befehle", value=cmd_desc)
-
-            await ctx.response.send_message(embed=emb, ephemeral=not not_silent)
-            return
-        selection = selection.strip()
-        if selection in self.state.bot.cogs:
-            cog = self.state.bot.cogs[selection]  # type: commands.Cog
-            emb = discord.Embed(title=f"Hilfe zu {cog.__cog_name__}", color=discord.Color.red(),
-                                description="Gib `/help <Befehl>` oder `/help <Modul>` ein für weitere Informationen.\n"
-                                            "Bei den Befehlsdetails werden optionale Parameter mit <`Param`> und "
-                                            "verpflichtende Parameter mit [`Param`] gekennzeichnet")
             for cmd in cog.walk_commands():
-                cmd_name = f"{cmd.full_parent_name} {cmd.name}".strip()
-                cmd_desc = cmd.description if cmd.description is not None and len(cmd.description) > 0 else "N/A"
-                if isinstance(cmd, SlashCommand):
-                    if len(cmd.options) > 0:
-                        cmd_desc += "*Parameter*:\n"
-                    for opt in cmd.options:
-                        # noinspection PyUnresolvedReferences
-                        cmd_desc += f"  {'[' if opt.required else '<'}`{opt.name}`{']' if opt.required else '>'}: " \
-                                    f"`{opt.input_type.name}` - {opt.description}\n"
-                emb.add_field(name=cmd_name, value=cmd_desc, inline=False)
-            await ctx.response.send_message(embed=emb, ephemeral=not not_silent)
-            return
+                cmd_desc += f"`{get_cmd_name(cmd)}`: {cmd.description}\n"
+            emb.add_field(name=name, value=cmd_desc, inline=False)
+        cmd_desc = ""
+        for cmd in bot.walk_commands():
+            if not cmd.cog_name and not cmd.hidden:
+                cmd_desc += f"{get_cmd_name(cmd)} - {cmd.description}\n"
+        if cmd_desc:
+            emb.add_field(name="Andere Befehle", value=cmd_desc)
+        return emb
+
+    @staticmethod
+    def get_cog_embed(cog: commands.Cog):
+        emb = discord.Embed(title=f"Hilfe zu {cog.__cog_name__}", color=discord.Color.red(),
+                            description="Gib `/help <Befehl>` oder `/help <Modul>` ein für weitere Informationen.\n"
+                                        "Bei den Befehlsdetails werden optionale Parameter mit `<Name: Type>` und "
+                                        "verpflichtende Parameter mit `[Name: Type]` gekennzeichnet.\n\n"
+                                        "Verfügbare Befehle:")
+        for cmd in cog.walk_commands():
+            cmd_name = get_cmd_name(cmd)
+            cmd_desc = cmd.description if cmd.description is not None and len(cmd.description) > 0 else "N/A"
+            if isinstance(cmd, SlashCommand):
+                if len(cmd.options) > 0:
+                    cmd_desc += " **Parameter**:\n"
+                for opt in cmd.options:
+                    # noinspection PyUnresolvedReferences
+                    cmd_desc += f"`{'[' if opt.required else '<'}{opt.name}: {opt.input_type.name}" \
+                                f"{']' if opt.required else '>'}`: {opt.description}\n"
+            emb.add_field(name=cmd_name, value=cmd_desc, inline=False)
+        return emb
+
+    @staticmethod
+    def get_command_embed(command: commands.Command):
+        description = "Keine Beschreibung verfügbar"
+        if command.description is not None and len(command.description) > 0:
+            description = command.description
+        emb = discord.Embed(title=f"Hilfe zu `{get_cmd_name(command)}`", color=discord.Color.red(),
+                            description=description)
+        if isinstance(command, SlashCommand):
+            if len(command.options) > 0:
+                description += "\nParameter:"
+            for opt in command.options:
+                # noinspection PyUnresolvedReferences
+                emb.add_field(name=opt.name,
+                              value=f"{'(Optional)' if not opt.required else '(Benötigt)'}: `{opt.input_type.name}`\n"
+                                    f"Default: `{str(opt.default)}`\n{opt.description}",
+                              inline=False)
+        emb.description = description
+        return emb
+
+    @staticmethod
+    def get_help_embed(bot: commands.Bot, selection: Optional[str] = None):
+        if selection is None:
+            return HelpCommand.get_general_embed(bot)
+        selection = selection.strip()
+        if selection in bot.cogs:
+            cog = bot.cogs[selection]
+            return HelpCommand.get_cog_embed(cog)
         command = None
-        for cmd in self.state.bot.walk_commands():
-            if f"{cmd.full_parent_name} {cmd.name}".strip().casefold() == selection.casefold():
+        for cmd in bot.walk_commands():
+            if f"{get_cmd_name(cmd)}".casefold() == selection.casefold():
                 command = cmd
                 break
         if command is None:
-            for cog in self.state.bot.cogs.values():
+            for cog in bot.cogs.values():
                 for cmd in cog.walk_commands():
-                    if f"{cmd.full_parent_name} {cmd.name}".strip().casefold() == selection.casefold():
+                    if f"{get_cmd_name(cmd)}".casefold() == selection.casefold():
                         command = cmd
                         break
                 if command is not None:
                     break
         if command is not None:
-            description = "Keine Beschreibung verfügbar"
-            if command.description is not None and len(command.description) > 0:
-                description = command.description
-            if isinstance(command, SlashCommand):
-                if len(command.options) > 0:
-                    description += "\nParameter:"
-                emb = discord.Embed(title=f"Hilfe zu `{command.name}`", color=discord.Color.red(),
-                                    description=description)
-                for opt in command.options:
-                    # noinspection PyUnresolvedReferences
-                    emb.add_field(name=opt.name,
-                                  value=("(Optional):" if not opt.required else "(Benötigt): ") +
-                                  f"{opt.input_type.name}\nStandardwert: '{str(opt.default)}\n{opt.description}'",
-                                  inline=False)
+            return HelpCommand.get_command_embed(command)
+        return discord.Embed(title=f"Hilfe", color=discord.Color.red(),
+                             description=f"Befehl/Modul `{selection}` nicht gefunden. Gib `/help` ein um eine Liste "
+                                         f"aller Befehle zu sehen.")
 
-            else:
-                emb = discord.Embed(title=f"Hilfe zu {command.name}", color=discord.Color.red(),
-                                    description=description)
-            await ctx.response.send_message(embed=emb, ephemeral=not not_silent)
-            return
-
-        emb = discord.Embed(title=f"Hilfe", color=discord.Color.red(),
-                            description=f"Befehl/Modul `{selection}` nicht gefunden. Gib `/help` ein um eine Liste "
-                                        f"aller Befehle zu sehen.")
-        await ctx.response.send_message(embed=emb, ephemeral=not not_silent)
+    @commands.slash_command(name="help", description="Help-Command")
+    @option(name="selection", description="The command/module to get help about.", type=str, required=False,
+            autocomplete=commands_autocomplete)
+    @option(name="silent", description="Execute the command publicly.", type=bool, required=False, default=True,
+            autocomplete=commands_autocomplete)
+    @option(name="edit_msg", description="Edit this message and update the embed", type=int, required=False,
+            default=None)
+    async def help(self, ctx: ApplicationContext,
+                   selection: str, silent: bool, edit_msg: int
+                   ):
+        emb = HelpCommand.get_help_embed(self.state.bot, selection)
+        if edit_msg is not None:
+            await ctx.response.defer(ephemeral=silent)
+            try:
+                msg = await ctx.channel.fetch_message(edit_msg)
+                await msg.edit(embed=emb)
+                await ctx.followup.send("Message edited", ephemeral=silent)
+            except discord.NotFound:
+                await ctx.followup.send("Message not found in current channel", ephemeral=silent)
+        await ctx.response.send_message(embed=emb, ephemeral=silent)
 
 
 class BaseCommands(commands.Cog):
@@ -222,7 +242,7 @@ class BaseCommands(commands.Cog):
         await ctx.defer(ephemeral=True)
         await sheet.load_wallets(force)
         if not user:
-            user_id = ctx.author.id
+            user_id = ctx.user.id
         else:
             user_id = user.id
 
@@ -282,10 +302,10 @@ class BaseCommands(commands.Cog):
             return
         await ctx.defer(ephemeral=True)
         users = await ctx.guild \
-                         .fetch_members() \
-                         .filter(lambda m: m.get_role(role.id) is not None) \
-                         .map(lambda m: (m.nick if m.nick is not None else m.name, m)) \
-                         .flatten()
+            .fetch_members() \
+            .filter(lambda m: m.get_role(role.id) is not None) \
+            .map(lambda m: (m.nick if m.nick is not None else m.name, m)) \
+            .flatten()
         unreg_users = []
         old_users = []
         for name, user in users:  # type: str, discord.Member
